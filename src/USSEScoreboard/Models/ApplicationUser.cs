293a2430906aaf7666e2f091bf601669e01d1052
Joe Raio <Joe.Raio@Microsoft.com>
2016-07-05 16:04:53 -0400

    Some misc cleanup..

--- src/USSEScoreboard/Models/ApplicationUser.cs
+++ src/USSEScoreboard/Models/ApplicationUser.cs
@@ -10,15 +10,6 @@ namespace USSEScoreboard.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-        //[StringLength(100)]
-        //public string FirstName { get; set; }
-
-        //[StringLength(100)]
-        //public string LastName { get; set; }
-        //public string FullName {
-        //    get { return FirstName + " " + LastName; }
-        //}
-
         public ICollection<UserProfile> UserProfile { get; set; }
 
     }
--- src/USSEScoreboard/Models/CommitmentViewModels/ListCommitmentsViewModel.cs
+++ src/USSEScoreboard/Models/CommitmentViewModels/ListCommitmentsViewModel.cs
@@ -11,25 +11,5 @@ namespace USSEScoreboard.Models.CommitmentViewModels
     {
         public IEnumerable<Commitment> Commitments { get; set; }
 
-        //public enum CommitmentStatus
-        //{
-        //    [Description("Not Started")]
-        //    NotStarted,
-        //    [Description("In Progress")]
-        //    InProgress,
-        //    [Description("Completed")]
-        //    Completed
-        //}
-        //public class Commitment
-        //{
-        //    public int Id { get; set; }
-        //    public string Title { get; set; }
-        //    public string Description { get; set; }
-        //    public string Status { get; set; }
-        //    public DateTime DateCreated { get; set; }            
-        //    //public ApplicationUser User { get; set; }
-        //    public string UserId { get; set; }
-        //    public string FullName { get; set; }
-        //}
     }
 }

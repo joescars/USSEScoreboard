274e4ce6afe197d95fc83f860d9138f9aa8a4edf
Joe Raio <joe.raio@microsoft.com>
2017-04-06 10:34:33 -0400

    Unit test project holder. Basic Test

---
+++ Website/USSEScoreboard.Tests/ProgressCalculator_Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using USSEScoreboard.Interfaces;
+using USSEScoreboard.Models;
+using Xunit;
+
+namespace USSEScoreboard.Tests
+{
+    public class ProgressCalculator_Tests
+    {
+        //TODO: Add dependency injection for wigsettings from db
+
+        [Fact]
+        public void ResultsValue1()
+        {
+            //setup
+            var StartDate = new DateTime(2016, 7, 1);
+            var EndDate = new DateTime(2017, 7, 1);
+            var AWinGoal = 20;
+            var CWinGoal = 100;
+            var TotalAscendWins = 10;
+            var TotalPresentations = 20;
+
+            ProgressCalculator pg = new ProgressCalculator();
+            ProgressCalculator.Result result = pg.CalculateProgress(
+                StartDate, EndDate, AWinGoal,
+                CWinGoal, TotalAscendWins, TotalPresentations);
+            Assert.True(result.AscendOverallPct > 1,"Percentage should be greater than 0");
+        }
+    }
+}
